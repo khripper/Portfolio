8c2ef4f9457f24125a770baf097ff4e7bd4579b6
khripper <111386720+khripper@users.noreply.github.com>
2023-03-21 17:10:49 +0000

    Creating Componetns

---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Nav.scss';
+const Nav = () => {
+  return (
+    <div>Nav</div>
+  )
+}
+
+export default Nav
---
+++ src/Components/Portfolio/Porfolio.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Portfolio.scss'
+
+const Porfolio = () => {
+  return (
+    <div>Porfolio</div>
+  )
+}
+
+export default Porfolio
